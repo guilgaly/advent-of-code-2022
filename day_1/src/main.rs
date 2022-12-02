6ea3d3ac88ddabebbacd626fe1ad2e17f6bd293c
Guillaume Galy <guilgaly@gmail.com>
2022-12-02 07:41:29 +0100

    Day 1

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,9 +1,38 @@
+use common::itertools::Itertools;
 use std::error::Error;
+use std::num::ParseIntError;
 
 static INPUT: &str = include_str!("input");
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let elves = parse_input(INPUT)?;
+
+    let max = find_max_calories(&elves);
+    println!("Part 1 result: {}", max);
+
+    let top_three_sum = find_top_three_sum(&elves);
+    println!("Part 2 result: {}", top_three_sum);
     Ok(())
+}
+
+fn find_max_calories(elves: &[Vec<u64>]) -> u64 {
+    elves.iter().map(|elf| elf.iter().sum()).max().unwrap_or(0)
+}
+
+fn find_top_three_sum(elves: &[Vec<u64>]) -> u64 {
+    elves
+        .iter()
+        .map(|elf| elf.iter().sum())
+        .sorted_by(|a: &u64, b: &u64| Ord::cmp(&b, &a))
+        .take(3)
+        .sum()
+}
+
+fn parse_input(input: &str) -> Result<Vec<Vec<u64>>, ParseIntError> {
+    input
+        .split("\n\n")
+        .map(|elf| elf.lines().map(|line| line.parse::<u64>()).collect())
+        .collect()
 }
 
 #[cfg(test)]
